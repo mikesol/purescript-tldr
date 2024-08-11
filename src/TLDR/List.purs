545447757f883f424b6d06a562e58f0227ee83ab
Mike Solomon <mike.solomon@hey.com>
2024-08-11 07:02:19 +0300

    Adds cons

--- src/TLDR/List.purs
+++ src/TLDR/List.purs
@@ -11,3 +11,5 @@ class Append l1 l2 l3 | l1 l2 -> l3
 instance Append Nil l l
 else instance Append l Nil l
 else instance (Append tail l3 l4) => Append (Cons head tail) l3 (Cons head l4)
+
+infixr 6 type Cons as :
